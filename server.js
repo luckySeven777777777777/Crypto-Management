0726bd9cabf361025963f23b1b0e333006923fe4
luckySeven777777777777 <lucky077779999@gmail.com>
2025-12-11 17:31:31 +0630

    Update server.js

--- server.js
+++ server.js
@@ -784,7 +784,6 @@ function objToSortedArray(obj) {
 /* ---------------------------------------------------------
    Server Start
 --------------------------------------------------------- */
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("🚀 Server running on port", PORT);
 });
